bbd5397faa28e917fa90e908f39a64343a27f73c
Sara <hey@iamsaravieira.com>
2019-11-13 12:59:03 +0100

    remove json transform

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,12 +29,6 @@ module.exports = {
       },
     },
     `gatsby-transformer-json`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `./shops/`,
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
